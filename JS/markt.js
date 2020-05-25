eba158fb246fdd1ec2ffd821ce27a0bfeac8e0ed
Hanno Kügler <hannokuegler@gmailcom>
2020-05-25 11:42:54 +0200

    markt #5

---
+++ JS/markt.js
@@ -0,0 +1,17 @@
+
+// Wird ausgeführt, wenn webseite geladen hat.
+window.addEventListener('load', function () {
+
+  var kurs = 288.00;
+
+  document.getElementByID("applekurs").innerHTML() = kurs;
+//getAppleData();
+  window.alert("Gruezi miteinond");
+});
+
+function getAppleData()
+{
+    var kurs = 288.00;
+    document.getElementByID("Applekurs").innerHTML = kurs;
+}
+  window.alert("Gruezi miteinond");
