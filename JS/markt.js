74feb87faf2462f9bb91af8ef832cc6a4992558d
Hanno Kügler <hannokuegler@gmailcom>
2020-06-08 12:05:41 +0200

    markt

---
+++ JS/markt.js
@@ -0,0 +1,14 @@
+
+
+
+var appleKurs = document.getElementById("applekurs");
+appleKurs.innerHTML = 1000 + "€";
+
+var microsoftKurs = document.getElementById("microsoftkurs");
+microsoftKurs.innerHTML = 800 + "€";
+
+var netflixKurs = document.getElementById("netflixkurs");
+netflixKurs.innerHTML = 600 + "€";
+
+var cocacolaKurs = document.getElementById("cocacolakurs");
+cocacolaKurs.innerHTML = 400 + "€";
